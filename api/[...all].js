b004be501688776d740a0d105c101875091af90a
scooper1980 <oluwaseyifapohunda@gmail.com>
2026-06-29 16:17:29 +0100

    Normalize Vercel API paths for admin login

--- api/[...all].js
+++ api/[...all].js
@@ -1,3 +1,9 @@
 import app from "../Car Rentals/server/server.js";
 
-export default app;
+export default function handler(req, res) {
+  if (req?.url?.startsWith("/api")) {
+    req.url = req.url.replace(/^\/api/, "") || "/";
+  }
+
+  return app(req, res);
+}
